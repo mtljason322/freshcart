a34d095376624a508b9669c06dcdee22d7db51e0
Jordan <jordanmone@gmail.com>
2025-09-26 08:51:19 -0400

    fix:Fix dataclass constructor conflict and enforce mandatory price

---
+++ src/freshcart/__init__.py
@@ -0,0 +1,21 @@
+"""
+Freshcart - package principal
+Expose les objets importants pour éviter les imports trop longs.
+"""
+
+
+#from freshcart.domain.products import Product
+#on pourra faire :
+#from freshcart import Product
+
+
+
+from .domain.inventory import Inventory, ProductNotFoundError
+from .domain.products import PerishableProduct, Product
+
+__all__ = [
+    "Product",
+    "PerishableProduct",
+    "Inventory",
+    "ProductNotFoundError",
+]
